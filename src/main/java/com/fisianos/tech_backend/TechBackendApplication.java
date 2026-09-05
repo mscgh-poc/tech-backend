e2bc358413232add1a21d1a23e2c9918f636f25f
Mark Zuckerber <marckcito@unmsm.edu.pe>
2026-09-05 10:26:31 -0500

    feat: scaffold Spring Boot WebFlux project from Spring Initializr

---
+++ src/main/java/com/fisianos/tech_backend/TechBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fisianos.tech_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fisianos/tech_backend/TechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisianos.tech_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
